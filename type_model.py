ab1036dbdc0fb87b55a1c126d8fe3abdb7c37e93
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-07-18 07:10:20 +0100

    remove set damage methods on TypeModel

--- type_model.py
+++ type_model.py
@@ -16,11 +16,5 @@ class TypeModel(BaseModel):
     receives_damage: Dict[DamageCategory, List[str]] = Field(default_factory=default_damage_dict)
     deals_damage: Dict[DamageCategory, List[str]] = Field(default_factory=default_damage_dict)
 
-    def set_defensive_relationship(self, damage_category: DamageCategory, type_list: List[PokemonType]):
-        self.receives_damage[damage_category] = type_list
-
-    def set_offensive_relationship(self, damage_category: DamageCategory, type_list: List[PokemonType]):
-        self.deals_damage[damage_category] = type_list
-
     def __str__(self):
         return f"Type: {self.name}"
