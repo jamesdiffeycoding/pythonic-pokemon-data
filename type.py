3bd6530654b2d83489b7b9af3ab527f36843b37d
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-07-18 06:56:35 +0100

    pydantic Literal for PokemonType

--- type.py
+++ type.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import List, Dict, Literal
 
 DamageCategory = Literal["super_effective", "not_very_effective", "immune"]
+PokemonType = Literal[ "Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy" ]
 
 def default_damage_dict():
     return {
@@ -15,10 +16,10 @@ class Type(BaseModel):
     receives_damage: Dict[DamageCategory, List[str]] = Field(default_factory=default_damage_dict)
     deals_damage: Dict[DamageCategory, List[str]] = Field(default_factory=default_damage_dict)
 
-    def set_defensive(self, damage_category: DamageCategory, type_list: List[str]):
+    def set_defensive_relationship(self, damage_category: DamageCategory, type_list: List[PokemonType]):
         self.receives_damage[damage_category] = type_list
 
-    def set_offensive(self, damage_category: DamageCategory, type_list: List[str]):
+    def set_offensive_relationship(self, damage_category: DamageCategory, type_list: List[PokemonType]):
         self.deals_damage[damage_category] = type_list
 
     def __str__(self):
