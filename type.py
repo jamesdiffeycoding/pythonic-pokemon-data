63b4582413508ec22700a5e0615067e1dd4e0cd6
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-07-18 06:43:53 +0100

    add pydantic

--- type.py
+++ type.py
@@ -1,33 +1,36 @@
-class Type:
-    def __init__(self, name):
-        self.name = name
-        self.receives_damage = {
-            "super_effective": [],
-            "not_very_effective": [],
-            "immune": []
-        }
-        self.deals_damage = {
-            "super_effective": [],
-            "not_very_effective": [],
-            "immune": []
-        }
+from pydantic import BaseModel, Field
+from typing import List, Dict
 
-    def set_defensive_weaknesses(self, type_list):
+
+class Type(BaseModel):
+    name: str
+    receives_damage: Dict[str, List[str]] = Field(default_factory=lambda: {
+        "super_effective": [],
+        "not_very_effective": [],
+        "immune": []
+    })
+    deals_damage: Dict[str, List[str]] = Field(default_factory=lambda: {
+        "super_effective": [],
+        "not_very_effective": [],
+        "immune": []
+    })
+
+    def set_defensive_weaknesses(self, type_list: List[str]):
         self.receives_damage["super_effective"] = type_list
 
-    def set_defensive_resistances(self, type_list):
+    def set_defensive_resistances(self, type_list: List[str]):
         self.receives_damage["not_very_effective"] = type_list
 
-    def set_defensive_immunities(self, type_list):
+    def set_defensive_immunities(self, type_list: List[str]):
         self.receives_damage["immune"] = type_list
 
-    def set_offensive_strengths(self, type_list):
+    def set_offensive_strengths(self, type_list: List[str]):
         self.deals_damage["super_effective"] = type_list
 
-    def set_offensive_ineffectivenesses(self, type_list):
+    def set_offensive_ineffectivenesses(self, type_list: List[str]):
         self.deals_damage["not_very_effective"] = type_list
 
-    def set_offensive_immunities(self, type_list):
+    def set_offensive_immunities(self, type_list: List[str]):
         self.deals_damage["immune"] = type_list
 
     def __str__(self):
