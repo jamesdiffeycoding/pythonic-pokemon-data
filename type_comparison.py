2a0a99fb872d94f4e04cbbebb9c19a8a6284cc07
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-07-18 08:19:05 +0100

    add super effective type comparison

--- type_comparison.py
+++ type_comparison.py
@@ -3,13 +3,27 @@ from type_instances import all_types
 from type_model import DamageCategory
 from typing import get_args
 
-print("--")
+ranked_types_detailed = [{
+        "name": t.name,
+        "deals_super_effective": len(t.deals["super_effective"]),
+        "deals_not_very_effective": len(t.deals["not_very_effective"]),
+        "deals_immune": len(t.deals["immune"]),
+        "receives_super_effective": len(t.receives["super_effective"]),
+        "receives_not_very_effective": len(t.receives["not_very_effective"]),
+        "receives_immune": len(t.receives["immune"]),
+    } for t in all_types]
 
+sorting_category = "deals_super_effective"
+
+ranked_types_detailed_sorted = sorted(
+    ranked_types_detailed,
+    key=lambda x: x[sorting_category],
+    reverse=True
+)
 
 if __name__ == "__main__":
     log_main_file()
-    for type in all_types:
-        for category in get_args(DamageCategory):
-            print(f"\n{type} - {category}")
-            print(f"deals: {type.deals[category]}")
-            print(f"receives: {type.receives[category]}")
+    print(f"data sorted by - {sorting_category}")
+    for t in ranked_types_detailed_sorted:
+        print(f"{t['name']} {t[sorting_category]}")
+
